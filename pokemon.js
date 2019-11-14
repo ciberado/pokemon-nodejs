dc95b9ae3de8f1c78b45b7c5199b37ee8a56d3e8
Ubuntu <ciberado@workstation.vbfxdgf4lh1ethkjbis0c3fn4e.ax.internal.cloudapp.net>
2019-11-14 16:18:10 +0000

    Integrated both with sqlserver and vault

--- pokemon.js
+++ pokemon.js
@@ -1,20 +1,30 @@
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
 const sql = require('mssql')
 
-const c = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:lab10829-pokemondb-server.database.windows.net,1433;Database=pokemonDB;Uid=dbadmin@lab10829-pokemondb-server;Pwd=;Encrypt=yes;TrustServerCertificate=no;';
-const connAttr = new Map(c.split(';').map(l => [l.split('=')[0].toLowerCase(), l.split('=')[1]]));
-
-const config = {
-  user: 'dbadmin',
-  password: 'Bragasdeesparto1',
-  server: connAttr.get('server').match('.*:(.*),')[1], 
-  database: connAttr.get('database'),
-  encrypt : true
-};
-
+if (!process.env.KEY_VAULT_URI) {
+  console.error('Please, define KEY_VAULT_URI environment variable. '+
+    'For example: \nexport KEY_VAULT_URI=https://lab-vault.vault.azure.net');
+  process.exit(-1);
+}
 
 async function getRandomPokemon() {
   let pool;
   try {
+    const credentials = new DefaultAzureCredential();
+    const client = new SecretClient(process.env.KEY_VAULT_URI, credentials);
+    const secret = await client.getSecret('PokemonDBConn');
+    const c = secret.value;
+    const connAttr = new Map(c.split(';').map(l => [l.split('=')[0].toLowerCase(), l.split('=')[1]]));
+
+    const config = {
+      user: connAttr.get('uid'),
+      password: connAttr.get('pwd'),
+      server: connAttr.get('server').match('.*:(.*),')[1], 
+      database: connAttr.get('database'),
+      encrypt : true
+    };
+    
     const query="SELECT TOP 1 id, name FROM pokemon ORDER BY NEWID()";
     pool = await sql.connect(config);
     const result = await pool.request().query(query);
