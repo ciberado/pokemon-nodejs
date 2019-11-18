63d7c050ec41e325009b6a4a9a07620d116b792e
Ubuntu <ciberado@workstation.vbfxdgf4lh1ethkjbis0c3fn4e.ax.internal.cloudapp.net>
2019-11-18 19:56:03 +0000

    Added ifconfig.co as alternative for public ip metadata

--- healthbeat.js
+++ healthbeat.js
@@ -9,29 +9,45 @@ const INTERVAL_IN_SECONDS = 5;
 let queueClient;
 const info = {};
 
-async function getNodeIP() {
-    // https://docs.microsoft.com/es-es/azure/virtual-machines/windows/instance-metadata-service
-    const options = {
-      uri: 'http://169.254.169.254/metadata/instance/network',
-      qs : {
-        'api-version' : '2017-08-01'
-      },
-      headers : {
-          'Metadata' : 'true'
-      },
-      json : true
-    };
+async function getNodeIPIfconfigCo() {
+  const options = {
+    uri: 'http://ifconfig.co'
+  };
+  const publicIp = await rp(options);
+  return publicIp;
+}
 
-    let publicIp;
-    do {
-      const result = await rp(options);
-      publicIp = result.interface[0].ipv4.ipAddress[0].publicIpAddress;
-      if (!publicIp) {
-        console.log(`Still waiting for public ip. Retrying in 20 seconds.`);
-        await delay(20*1000);
-      }
-    } while(!publicIp);
-    return publicIp;
+async function getNodeIPMetadata() {
+  // https://docs.microsoft.com/es-es/azure/virtual-machines/windows/instance-metadata-service
+  const options = {
+    uri: 'http://169.254.169.254/metadata/instance/network',
+    qs : {
+      'api-version' : '2017-08-01'
+    },
+    headers : {
+        'Metadata' : 'true'
+    },
+    json : true
+  };
+
+  const result = await rp(options);
+  const publicIp = result.interface[0].ipv4.ipAddress[0].publicIpAddress;
+  return publicIp;
+}
+
+async function getNodeIP() {
+  let publicIp;
+  do {
+    publicIp = await getNodeIPMetadata();
+    if (!publicIp) {
+      publicIp = await getNodeIPIfconfigCo();
+    }
+    if (!publicIp) {
+      console.log(`Still waiting for public ip. Retrying in 20 seconds.`);
+      await delay(20*1000);
+    }
+  } while(!publicIp);
+  return publicIp;
 }
 
 async function emitHealthbeat(beat) {
